24686d9515269d8ab6cd0fa2ad8f538cf4342bad
Aditya-Gwari <adityagwari3112008@gmail.com>
2026-09-08 22:41:49 +0530

    update file organiser

--- file_organizer.py
+++ file_organizer.py
@@ -24,24 +24,62 @@ file_types = {
     ".wav": "Music",
     ".flac": "Music"
 }
+def sort_folder():
 
-s = input("Enter the file path : ")
+    path_str = input("Enter the path to the file folder : ")
 
-p = Path(s)
+    path = Path(path_str)
 
-print(p)
+    if not path.is_dir():
+        print("Wrong path!!")
+        return
+    
+    for i in path.glob("*"):
 
-exts = []
+        if not i.is_dir():
+            ext = i.suffix
 
-for i in p.glob("*"):
+            fld = file_types.get(ext.lower() , "others")
+            dest = path  / fld
+            dest.mkdir(parents = True, exist_ok = True)
 
-    if not i.is_dir():
-        ext = i.suffix
+            shutil.move(i, dest)
+    print("All files in the folder have been sorted...")
 
-        fld = file_types.get(ext, "others")
-        dest = p  / fld
-        dest.mkdir(parents = True, exist_ok = True)
+def sort_file():
 
-        m = file_types.get(i, "others")
-        for l in p.glob("*"+ext):
-            shutil.move(l, p / m)
+    path_str = input("Enter the path to the file : ")
+
+    path = Path(path_str)
+
+    if not path.exists():
+        print("Wrong path!!")
+        return
+
+    ext = path.suffix
+    fld = file_types.get(ext.lower() , "others")
+
+    folder = path.parent
+
+    dest = folder / fld
+
+    dest.mkdir(parents = True, exist_ok = True)
+    try:
+        shutil.move(path, dest)
+        print("The file has been sorted...")
+    except shutil.Error as e:
+        print("The file already exists in your target folder...")
+
+        n = input("Would you like to REPLACE (1) it or RENAME (2) it? : ")
+        if n == "1":
+            path.replace(dest / path.name)
+            print("File Replaced!")
+
+        elif n == "2":
+            new_path = path.parent / f"{path.stem}(1){path.suffix}"
+            path.rename(new_path)
+            
+            
+            print("File Renamed and Sorted!!")
+
+sort_file()
