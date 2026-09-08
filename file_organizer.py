30c22d54c0293b51510c45a69c114e7b769d8888
Aditya-Gwari <adityagwari3112008@gmail.com>
2026-09-08 16:02:49 +0530

    Make the code efficient

--- file_organizer.py
+++ file_organizer.py
@@ -37,15 +37,11 @@ for i in p.glob("*"):
 
     if not i.is_dir():
         ext = i.suffix
-        if ext not in exts:
-            exts.append(ext)
 
-        ext = i.suffix
         fld = file_types.get(ext, "others")
         dest = p  / fld
         dest.mkdir(parents = True, exist_ok = True)
 
-for i in exts:
-    m = file_types.get(i, "others")
-    for l in p.glob("*"+i):
-        shutil.move(l, p / m)
+        m = file_types.get(i, "others")
+        for l in p.glob("*"+ext):
+            shutil.move(l, p / m)
